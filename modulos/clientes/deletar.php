8a30f424a50b2a2dc833ea085c349adb6b85d51d
Aldy Salvino <aldysalvino@gmail.com>
2019-04-12 12:10:45 -0300

    Alteração no deletar Clientes

--- modulos/clientes/deletar.php
+++ modulos/clientes/deletar.php
@@ -1,7 +1,7 @@
 <?php
-$id = $_GET['id'];
+$id = (isset($_GET['id']) ? $_GET['id'] : '');
 
-$acao = $_GET['acao'];
+$acao = (isset($_GET['acao']) ? $_GET['acao'] : '');
 
 
 include '../../config/mysql.php';
@@ -10,8 +10,9 @@ include '../../config/check.php';
 
 if ($acao == 'deletar')
 {
-
-	mysql_query("DELETE FROM clientes WHERE id=$id");
+  $sql = "DELETE FROM clientes WHERE id=$id";
+	mysqli_query($link,$sql);
+	
 	echo '<script>fecharpopup(); clientes(\'\'); </script>';
 	exit;
 		
